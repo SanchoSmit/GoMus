1eadbe3d4d786f9a0c1b88f9ce78e3714b86a7da
Sancho <Sanchosmit@gmail.com>
2016-04-11 23:34:11 +0300

    Added data model classes

--- app/src/main/java/ua/onpu/project/gomus/model/Location.java
+++ app/src/main/java/ua/onpu/project/gomus/model/Location.java
@@ -1,7 +1,125 @@
 package ua.onpu.project.gomus.model;
 
-/**
- * Created by Sancho96 on 11.04.2016.
- */
 public class Location {
+    private String name;
+    private String information;
+    private double lat;
+    private double lon;
+    private String image;
+    private float rating;
+
+    //Empty constructor
+    public Location() {
+    }
+
+    /**
+     * Getter for location name
+     * @return String name of location
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter for location name
+     * @param name String name of location
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Getter for information about location
+     * @return String information
+     */
+    public String getInformation() {
+        return information;
+    }
+
+    /**
+     * Setter for information about location
+     * @param information String information
+     */
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    /**
+     * Getter for latitude of location
+     * @return double latitude value
+     */
+    public double getLat() {
+        return lat;
+    }
+
+    /**
+     * Setter for latitude of location
+     * @param lat double latitude value
+     */
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    /**
+     * Getter for longitude of location
+     * @return double longitude value
+     */
+    public double getLon() {
+        return lon;
+    }
+
+    /**
+     * Setter for longitude of location
+     * @param lon double longitude value
+     */
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    /**
+     * Getter for location image path
+     * @return String path of image
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * Setter for location image path
+     * @param image String path of image
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * Getter for rating of location
+     * @return float rating value
+     */
+    public float getRating() {
+        return rating;
+    }
+
+    /**
+     * Setter for rating of location
+     * @param rating float rating value
+     */
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    /**
+     * Modified toString() method
+     * @return  String value of class fields
+     */
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", information='" + information + '\'' +
+                ", lat=" + lat +
+                ", lon=" + lon +
+                ", image='" + image + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/ua/onpu/project/gomus/model/Tour.java
+++ app/src/main/java/ua/onpu/project/gomus/model/Tour.java
@@ -1,7 +1,74 @@
 package ua.onpu.project.gomus.model;
 
-/**
- * Created by Sancho96 on 11.04.2016.
- */
+import java.util.ArrayList;
+
 public class Tour {
+    private String name;
+    private String image;
+    private ArrayList<Location> locations = new ArrayList<>();
+
+    //Empty constructor
+    public Tour() {
+    }
+
+    /**
+     * Getter for name of the tour
+     * @return  Name of the tour
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter for name of the tour
+     * @param name  String name of tour
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Getter for tour image path
+     * @return  String image path
+     */
+    public String getImage() {
+        return image;
+    }
+
+    /**
+     * Setter for tour image path
+     * @param image  String path of image
+     */
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * Getter for the list of locations
+     * @return  ArrayList of locations
+     */
+    public ArrayList<Location> getLocations() {
+        return locations;
+    }
+
+    /**
+     * Setter for list of locations
+     * @param locations  ArrayList of locations
+     */
+    public void setLocations(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
+
+    /**
+     * Modified toString() method
+     * @return  String value of class fields
+     */
+    @Override
+    public String toString() {
+        return "Tour{" +
+                "name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", locations=" + locations +
+                '}';
+    }
 }
