7fec8e8fc9d8ede57c0b5fd781ebf3ce0e2c0c29
Sancho <Sanchosmit@gmail.com>
2016-04-11 20:26:51 +0300

    Added packages structure and app icon

--- app/src/main/java/ua/onpu/project/gomus/activities/MainActivity.java
+++ app/src/main/java/ua/onpu/project/gomus/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package ua.onpu.project.gomus;
+package ua.onpu.project.gomus.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ua.onpu.project.gomus.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/ua/onpu/project/gomus/model/Tour.java
@@ -0,0 +1,7 @@
+package ua.onpu.project.gomus.model;
+
+/**
+ * Created by Sancho96 on 11.04.2016.
+ */
+public class Tour {
+}
