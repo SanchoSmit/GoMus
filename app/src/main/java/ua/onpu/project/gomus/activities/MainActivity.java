e7725bf638b05c4ad480692475a26089d7266722
Sancho <Sanchosmit@gmail.com>
2016-04-12 00:17:09 +0300

    Added ActionBar icons and colors

--- app/src/main/java/ua/onpu/project/gomus/activities/MainActivity.java
+++ app/src/main/java/ua/onpu/project/gomus/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package ua.onpu.project.gomus.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import ua.onpu.project.gomus.R;
 
