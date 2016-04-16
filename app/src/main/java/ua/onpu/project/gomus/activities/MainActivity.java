210f87836dcdeb6459fe0d77572f9d5075f0a13c
chernikovmn <chernikov2297@gmail.com>
2016-04-16 19:27:30 +0300

    Best Location Fix

--- app/src/main/java/ua/onpu/project/gomus/activities/MainActivity.java
+++ app/src/main/java/ua/onpu/project/gomus/activities/MainActivity.java
@@ -9,8 +9,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -26,8 +29,10 @@ public class MainActivity extends AppCompatActivity {
     private ToursAdapter adapter;
     private Toolbar toolbar;
     private CollapsingToolbarLayout collapsingToolbarLayout;
-    private LinearLayout bestLocation1;
-    private LinearLayout bestLocation2;
+    private RelativeLayout bestLocation1;
+    private RelativeLayout bestLocation2;
+    private ImageView bestLocationImage1;
+    private ImageView bestLocationImage2;
     private TextView bestLocationText1;
     private TextView bestLocationText2;
     private ArrayList<Tour> tours;
@@ -48,8 +53,10 @@ public class MainActivity extends AppCompatActivity {
         collapsingToolbarLayout.setTitle(getResources().getString(R.string.tours));
 
         // Best locations initialization
-        bestLocation1 = (LinearLayout) findViewById(R.id.best_location1);
-        bestLocation2 = (LinearLayout) findViewById(R.id.best_location2);
+        bestLocation1 = (RelativeLayout) findViewById(R.id.best_location1);
+        bestLocation2 = (RelativeLayout) findViewById(R.id.best_location2);
+        bestLocationImage1 = (ImageView) findViewById(R.id.best_location_image1);
+        bestLocationImage2 = (ImageView) findViewById(R.id.best_location_image2);
         bestLocationText1 = (TextView) findViewById(R.id.best_location_text1);
         bestLocationText2 = (TextView) findViewById(R.id.best_location_text2);
 
@@ -83,8 +90,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -116,13 +121,12 @@ public class MainActivity extends AppCompatActivity {
     private void updateBestLocations() {
         for(Location location: locations) {
             if(location.getRating()==4.0){
-                //TODO change image .into() resourse
-                //Picasso.with(bestLocation1.getContext()).load(location.getImage()).into(bestLocation1);
-                bestLocationText2.setText(location.getName());
+                Picasso.with(bestLocationImage1.getContext()).load(location.getImage()).into(bestLocationImage1);
+                bestLocationText1.setText(location.getName());
             }
             else if(location.getRating()==5.0)
             {
-                //Picasso.with(bestLocation2.getContext()).load(location.getImage()).into(bestLocation2);
+                Picasso.with(bestLocationImage2.getContext()).load(location.getImage()).into(bestLocationImage2);
                 bestLocationText2.setText(location.getName());
             }
 
