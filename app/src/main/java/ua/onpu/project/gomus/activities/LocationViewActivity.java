640b8a8e184729282634bcc8e378503342812ece
chernikovmn <chernikov2297@gmail.com>
2016-04-20 22:48:47 +0300

    Added LocationViewActivity menu items

--- app/src/main/java/ua/onpu/project/gomus/activities/LocationViewActivity.java
+++ app/src/main/java/ua/onpu/project/gomus/activities/LocationViewActivity.java
@@ -2,11 +2,11 @@ package ua.onpu.project.gomus.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,14 +17,14 @@ import ua.onpu.project.gomus.model.Location;
 
 public class LocationViewActivity extends AppCompatActivity {
 
-    private Location location;
-    private ImageView location_image;
-    private TextView location_text;
-    private TextView location_name;
-    private TextView location_address;
-    private AppBarLayout appBarLayout;
     private Toolbar toolbar;
     private CollapsingToolbarLayout collapsingToolbarLayout;
+    private Location location;
+    private ImageView locationImage;
+    private TextView locationText;
+    private TextView locationAddress;
+    private TextView locationPhone;
+    private TextView locationWebsite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +35,48 @@ public class LocationViewActivity extends AppCompatActivity {
         Bundle bd = intent.getExtras();
 
         if(bd != null)
-        {
             location = (Location)bd.get("best_location");
-        }
-
-        //AppBarLayout initialization
-        appBarLayout = (AppBarLayout) findViewById(R.id.appBar_location);
 
         //Toolbar initialization
         toolbar = (Toolbar) findViewById(R.id.anim_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         // CollapsingToolbar initialization
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar_location);
         collapsingToolbarLayout.setTitle(location.getName());
 
         //Init resources
-        location_image = (ImageView) findViewById(R.id.location_image);
-        location_text = (TextView) findViewById(R.id.location_text);
-        location_address = (TextView) findViewById(R.id.location_address);
+        locationImage = (ImageView) findViewById(R.id.location_image);
+        locationText = (TextView) findViewById(R.id.location_text);
+        locationAddress = (TextView) findViewById(R.id.location_address);
+        locationPhone = (TextView) findViewById(R.id.location_phone);
+        locationWebsite = (TextView) findViewById(R.id.location_website);
 
         setActivityResources(location);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_location, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+
+                // Finish activity on back button pressed
+                finish();
+                break;
+            case R.id.button_map:
+
+                // TODO: Location Map Intent
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -63,8 +84,10 @@ public class LocationViewActivity extends AppCompatActivity {
      * @param loc Object of Location class
      */
     private void setActivityResources(Location loc) {
-        location_text.setText(loc.getInformation());
-        location_address.setText(loc.getAddress());
-        Picasso.with(LocationViewActivity.this).load(loc.getImage()).into(location_image);
+        locationText.setText(loc.getInformation());
+        locationAddress.setText(loc.getAddress());
+        locationPhone.setText(loc.getPhone());
+        locationWebsite.setText(loc.getWebsite());
+        Picasso.with(LocationViewActivity.this).load(loc.getImage()).into(locationImage);
     }
 }
